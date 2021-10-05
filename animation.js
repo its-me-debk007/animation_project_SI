41b30d3cac7984ed463dab51da8fc8a0cd3b2456
its-me-debk007 <debashish.joy@gmail.com>
2021-10-05 10:40:45 +0530

    added falling asteroids and a spaceship

--- animation.js
+++ animation.js
@@ -1,27 +1,43 @@
-var screen;
-screen = document.getElementById("container");
-screen.style.color = "red";
-screen.style.backgroundColor = "black";
-screen.style.width = "1000px";
-screen.style.height = "1000px";
+let ast1= document.getElementById("img1");
+let ast2= document.getElementById("img2");
+let ast3= document.getElementById("img3");
+let ast4= document.getElementById("img4");
+let ast5= document.getElementById("img5");
+let ast6= document.getElementById("img6");
 
-var specimen = document.getElementById("specimen");
-specimen.style.backgroundColor = "white";
-specimen.style.width = "5%";
-specimen.style.height = "5%";
+ast1.style.position = "absolute";
+ast2.style.position = "absolute";
+ast3.style.position = "absolute";
+ast4.style.position = "absolute";
+ast5.style.position = "absolute";
+ast6.style.position = "absolute";
 
-var pos = 10;
-specimen.style.left = pos + 'px';
-specimen.style.top = pos + 'px';
+let pos = 0;
+// specimen.style.left = pos + 'px';
+ast1.style.top = pos + 'px';
+ast2.style.top = pos + 'px';
+ast3.style.top = pos + 'px';
+ast4.style.top = pos + 'px';
+ast5.style.top = pos + 'px';
+ast6.style.top = pos + 'px';
 
-var time = setInterval(move, 10);
+let speed = Math.round(Math.random()*20);
+let timer = setInterval(move, speed);
 
 function move(){
-    if(pos == 950) {
-        clearInterval(time);
-        return;
+
+    if(pos == 510) {
+        // clearInterval(timer);
+        pos = 0;
     }
+
     pos++;
-    specimen.style.left = pos + 'px';
-    specimen.style.top = pos + 'px';
+    ast1.style.top = pos + 'px';
+    ast2.style.top = pos + 'px';
+    ast3.style.top = pos + 'px';
+    ast4.style.top = pos + 'px';
+    ast5.style.top = pos + 'px';
+    ast6.style.top = pos + 'px';
+
 }
+
